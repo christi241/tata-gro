0144132c0428c09d31204b45d185a002c5beb152
christi241 <christophermangoumbou5@gmail.com>
2024-03-05 05:04:50 +0000

    nouvelles modification sur le kubernettes7

---
+++ src/main/java/com/example/toto/main.java
@@ -0,0 +1,12 @@
+package com.example.toto;
+
+
+
+
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("bonjour tous le monde bienvenue sue le kubernate");
+
+    }
+}
